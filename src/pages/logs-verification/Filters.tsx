61799e1884958ed0dfdfcdafc7a5f4e11df27a11
GONCALO AZEVEDO <goncalosazevedo@gmail.com>
2024-04-29 17:53:55 +0100

    Alteração de filtros e de colunas na tabela

--- src/pages/logs-verification/Filters.tsx
+++ src/pages/logs-verification/Filters.tsx
@@ -1,5 +1,5 @@
 import { Filter, FilterForm, HiddenFilterForm } from '@asseco-web/ui'
-import { Button, Col, DatePicker, Input, Row, TimePicker } from '@asseco-web/ui/atomic'
+import { Col, Input, Row } from '@asseco-web/ui/atomic'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
@@ -12,8 +12,6 @@ export const Filters = ({ setFilters, filters }: { setFilters: Dispatch<SetState
     user: '',
     station: '',
     application: '',
-    datepicker: '',
-    timepicker: '',
   }
 
   const { control, handleSubmit, reset } = useForm({ mode: 'all', defaultValues: { ...defaultValues, ...filters } })
@@ -28,15 +26,12 @@ export const Filters = ({ setFilters, filters }: { setFilters: Dispatch<SetState
       <FilterForm onClear={clearSearch} onSubmit={handleSubmit(setFilters)}>
         <Row>
           <Input control={control} label={t('user')} name="user" />
-          <Input control={control} label={t('station')} name="station" />
-          <Input control={control} label={t('application')} name="application" />
         </Row>
         <HiddenFilterForm>
           <Row>
             <Col component="form" onSubmit={handleSubmit(setFilters)} alignItems="flex-start">
-              <DatePicker control={control} name="datepicker" label={t('Datepicker')} w={1 / 6} />
-              <TimePicker control={control} name="timepicker" label={t('Timepicker')} w={1 / 6} />
-              <Button type="submit">Submit</Button>
+              <Input control={control} label={t('station')} name="station" w={1 / 6} />
+              <Input control={control} label={t('application')} name="application" w={1 / 6} />
             </Col>
           </Row>
         </HiddenFilterForm>
